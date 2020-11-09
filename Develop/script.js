d1ad8366103e029b8a82b8763373a58b9f157204
andrew <ana71@georgetown.edu>
2020-11-09 00:00:34 -0600

    finalizing assignment

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// password conditions
-// number of characters in the password 
-var characterAmount = prompt("How many characters should your password have? Choose a number between 8 and 128.");
+// strings to be added to var characters if confirmed
+var characters = "";
+var lowerConfirm = "acdefghijklmnopqrstuvwxyz";
+var upperConfirm = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberConfirm = "1234567890";
+var specialCharacterConfirm = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+function generatePassword (characterAmount) {
+  var characterAmount = prompt("How many characters should your password have? Choose a number between 8 and 128.");
+  while (characterAmount < 8 || characterAmount > 128) {
+    alert("Please enter a number between 8 and 128.")
+    characterAmount = prompt("How many characters should your password have? Choose a number between 8 and 128.");
+}
+
 // lowercase
 var lowerCase = confirm("Click OK to include lowercase letters in your password. Click Cancel to exclude lowercase letters from your password.");
 // uppercase
@@ -13,44 +24,25 @@ var numbers = confirm("Click OK to include numbers in your password. Click Cance
 // special characters
 var specialCharacters = confirm("Click OK to include special characters in your password. Click Cancel to exclude special characters from your password.");
 
-var objLength = JSON.parse(characterAmount);
-var length = characterAmount
+  var objLength = JSON.parse(characterAmount);
+  var length = characterAmount;
 
-function generatePassword (characterAmount) {
   var result = "";
 
   if (lowerCase)  {
-    var characters = "acdefghijklmnopqrstuvwxyz"
-    if (upperCase) {
-      var characters = "acdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      if (numbers) {
-        var characters = "acdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        if (specialCharacters) {
-          var characters = "acdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-        }
-      }
-    }
-  }   
-
-  else if (upperCase) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if (numbers) {
-      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-      if (specialCharacters) {
-        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-      }
-    }
+    var characters = characters + lowerConfirm;
   }
 
-  else if (numbers) {
-    var characters = "1234567890"
-    if (specialCharacters) {
-      var characters = "1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    }
+  if (upperCase) {
+    var characters = characters + upperConfirm;
   }
 
-  else if (specialCharacters) {
-    var characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  if (numbers) {
+    var characters = characters + numberConfirm;
+  }
+
+  if (specialCharacters) {
+    var characters = characters + specialCharacterConfirm;
   }
 
   var charactersLength = characters.length;
@@ -59,11 +51,6 @@ function generatePassword (characterAmount) {
   }
   return result
 }
-
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
